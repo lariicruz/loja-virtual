2c1a5b4cb68f680700fa20ed33f7d17f823d10ce
Larissa Cruz <mlp-larissa@hotmail.com>
2021-12-08 18:35:30 -0300

    Inserindo dados na tela

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,83 @@
+import 'package:shop/models/product.dart';
+
+final dummyProducts = [
+  Product(
+    id: 'p1',
+    name: 'Reconstrutor Amávia',
+    description: 'O Reconstrutor Amávia Repair Complex traz em sua fórmula uma combinação perfeita do Complexo Bio Restore, ativo rico em carbocisteína e peptídeos do trigo, que melhoram a resistência e elasticidade dos cabelos, com a Creatina aminoácido capaz de penetrar no córtex capilar promovendo uma completa reconstrução dos fios danificados.',
+    price: 149.99,
+    imageUrl:
+        'https://tfcqb6.vteximg.com.br/arquivos/ids/156062-1000-1000/repair-complex.jpg?v=637534113207830000',
+  ),
+  Product(
+    id: 'p2',
+    name: 'Liso Nobre',
+    description: 'O kit Selante Amávia Liso Nobre foi desenvolvido com um bio-polímero natural que envolve a fibra capilar, promovendo uma perfeita selagem e alinhamento das cutículas. A presença de um complexo com 11 aminoácidos garante uma surpreendente hidratação interna da fibra capilar, aumentando sua resistência contra a ação do secador e prancha',
+    price: 119.99,
+    imageUrl:
+        'https://cf.shopee.com.br/file/460ca01846a5b6d5c0e07e7e6457688d',
+  ),
+  Product(
+    id: 'p3',
+    name: 'Amávia Antiqueda',
+    description: 'A linha Energy Complex da Amávia combina em sua fórmula um complexo capilar Tricapil, um concentrado de jaborandi, quina e artemia, mais a ação nutritiva e fortalecedora da Arginina e Vitamina E. Como o  Kit Antiqueda e Crescimento Capilar Energy Complex da Amávia esses ativos agem juntos para combater as causas da perda capilar, além de fortalecer e estimular o crescimento dos fios. ',
+    price: 139.99,
+    imageUrl: 'https://a-static.mlcdn.com.br/1500x1500/kit-antiqueda-e-crescimento-capilar-energy-complex-amavia/srtacarvalhomaquiagemecosmeticos/8829732982/770f28066eea13986546b2d50985f69b.jpg',
+  ),
+  Product(
+    id: 'p4',
+    name: 'Amávia Cachos',
+    description: 'O Kit Hidratante Amávia Cacho Sublime, combina a extraordinária ação reparadora e hidratante do óleo de argan, rico em ácidos graxos essênciais e vitamina e, com as propriedades nutritivas e modeladoras da proteína do colágeno.',
+    price: 99.99,
+    imageUrl:
+        'https://static3.tcdn.com.br/img/img_prod/874112/kit_make_curl_gelatina_umidificador_spray_umectante_amavia_581_1_20201214024417.png',
+  ),
+  Product(
+    id: 'p5',
+    name: 'Hidratação Amakha',
+    description: 'O kit Hidratante é um produto com efeito desmaia cabelo, e com ativos incríveis. Cabelos mais fortes, com brilho e macios.',
+    price: 129.99,
+    imageUrl:
+        'https://images-americanas.b2w.io/produtos/01/00/img/1644212/0/1644212033_1GG.jpg',
+  ),
+  Product(
+    id: 'p6',
+    name: 'Reconstrução Mel',
+    description: 'Mel Capilar Amakha Paris, é enriquecida com mel e geleia real que possuem propriedades nutritivas que reconstroem a fibra capilar danificada pela ação química, conferindo um brilho radiante. Também possui em sua formula o colágeno, que atua na reposição hídrica dos fios, aumentando a sua resistência, resultando em cabelos mais fortes e saudáveis.',
+    price: 149.99,
+    imageUrl:
+        'https://cf.shopee.com.br/file/62cf2a87fcc1563da8ee180afef34b4d_tn',
+  ),
+  Product(
+    id: 'p7',
+    name: 'Teia Caviar',
+    description: 'A Linha Pantovin Teia Caviar, é um tratamento reconstrutor que repara profundamente os cabelos danificados alinhando as deficiências da cutícula e devolvendo os nutrientes vitais perdidos por cabelos seriamente danificados. Hidrata, repara e protege os cabelos enquanto reconstrói. e é possível ver resultados já na primeira aplicação.',
+    price: 169.99,
+    imageUrl:
+        'https://http2.mlstatic.com/D_NQ_NP_731787-MLB44877532414_022021-W.jpg',
+  ),
+  Product(
+    id: 'p8',
+    name: 'Tônico Crescimento',
+    description: 'O tônico nano vegetal pantovin é um tratamento de ação intensa, que equilibra o ph capilar, tem ação adstringente, anti-oleosidade, anti-séptica, antioxidante, cicatrizante, combate a caspa e formação de microorganismos, é vasodilatador, ou seja aumenta a circulação sanguínea em torno do folículo piloso, aumentando assim, a oxigenação do bulbo capilar, nutrindo, fortalecendo e revitalizando os fios.',
+    price: 49.99,
+    imageUrl:
+        'https://www.pantovinthreetherapy.com.br/wp-content/uploads/2019/11/Design-sem-nome-10.png',
+  ),
+  Product(
+    id: 'p9',
+    name: 'Máscara senscience',
+    description: 'Senscience, é uma Máscara Reparadora Profunda Enriquecida com uma mistura exclusiva de emulsões de silicone dos Laboratórios Shiseido para repor a hidratação e os aminoácidos essenciais que promovem um cabelo saudável. Condicionadores poliméricos e umectantes proporcionam maciez excepcional, nutrindo os cabelos e equilibrando o nível de hidratação dos fios. Repara e sela as cutículas danificadas proporcionando maciez, brilho e aspecto saudável.',
+    price: 119.99,
+    imageUrl:
+        'https://salaovirtual.org/wp-content/uploads/2021/04/mascara-500-ml.jpg',
+  ),
+  Product(
+    id: 'p10',
+    name: 'Kit senscience',
+    description: 'Senscience True Hue foi desenvolvido para cabelos coloridos. Limpa suavemente e protege a cor dos fios, evitando o desbotamento, além de hidratar, desembaraçar e condicionador os fios, melhora também a elasticidade dos fios.Sua formula deixa os fios hidratados de maneira suave e protege do desbotamento, Possui proteção uv, o que mantém a densidade da cor por muito mais tempo. Seus fios limpos, hidratados, macios, luminosos e protegidos.',
+    price: 199.99,
+    imageUrl:
+        'https://prohair.vteximg.com.br/arquivos/ids/155594-1000-1000/kit-true-hue-violet-profissional.jpg?v=637262059153070000',
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
+import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'models/product_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Store Of Hair',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch() .copyWith(primary: Colors.black , secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        // home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.HOME: (ctx) => ProductsOverviewPage(),
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+          AppRoutes.CART: (ctx) => CartPage(),
+          AppRoutes.ORDERS: (ctx) => OrdersPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
